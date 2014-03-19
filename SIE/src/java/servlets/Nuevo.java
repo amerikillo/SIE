9f89f06d0bac24f6b00c7a2ade6ac543cabf40a3
amerikillo <chp.chino@gmail.com>
2014-03-19 17:37:33 -0600

    Conexion perdida

--- SIE/src/java/servlets/Nuevo.java
+++ SIE/src/java/servlets/Nuevo.java
@@ -122,6 +122,7 @@ public class Nuevo extends HttpServlet {
                                 sumaCompraInventario(rsetm.getString("clave"), rsetm.getString("cant"));
                                 consql.conectar();
                                 consql.insertar("insert into TB_Compra values ('C', '" + dame7car(Cla_Doc) + "', '" + Cla_Prv + "', 'A',  '  000', '" + df2.format(df.parse(rsetm.getString("date"))) + " 00:00:00', NULL, '" + rsetm.getString("clave") + "', '', NULL, '1', '" + rsetm.getString("cant") + "', '0', '" + PreCant + "', '0', '" + PreCant + "', '" + PreCant + "', '0', '" + Impuesto + "', '" + CompraTotal + "', '" + Precio + "', '" + idLote + "', 'D', '" + df2.format(df.parse(rsetm.getString("date"))) + " 00:00:00', '" + sesion.getAttribute("nombre") + "', '" + idObser + "', '" + idObser + "', '', '" + rsetm.getString("folio_remi") + "') ");
+                                consql.conectar();
                                 insertaMovimiento(Cla_Doc, rsetm.getString("clave"), rsetm.getString("cant"), Precio, CompraTotal, idLote, rsetm.getString("observaciones"), Cla_Prv);
                                 consql.conectar();
                                 insertaCompraBitacora(sesion.getAttribute("nombre").toString(), "COMPRA-MANUAL", "REGISTRAR", Cla_Doc, "1", "COMPRAS");
